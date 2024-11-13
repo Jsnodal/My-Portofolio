3f448226ebbf27f8d8be389028c06bffeac3fe39
Jsnodal <mwirigialex351@gmail.com>
2024-11-13 18:19:30 +0300

    prtofolio

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function showDetails(project) {
-    const projectDetails = {
-      energy: "This project focuses on using temporal features like time and temperature to predict daily energy consumption...",
-      drones: "The autonomous drone project involves developing a machine learning model to enable drone navigation in complex environments...",
-      insurance: "Insurance charge prediction app leverages customer attributes to estimate insurance charges accurately using regression models..."
-    };
-  
-    alert(projectDetails[project]);
-  }
-  
+ 
